7a306a1954e499c9531a864e8c91394a918e091c
Nikhila Badri <badrinikhilapatel56@gmail.com>
2021-11-26 16:10:05 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = (props) => {
+    return (
+        <div className='col'>
+            <h1>{props.heading}</h1>
+            
+        </div>
+    )
+}
